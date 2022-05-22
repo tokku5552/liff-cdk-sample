717000a857409abb449b590651f285d54e3228b0
tokku5552 <physics.0121@gmail.com>
2022-05-22 21:28:50 +0900

    update: liff-app-stack

--- lib/liff-cdk-sample-stack.ts
+++ lib/liff-cdk-sample-stack.ts
@@ -1,16 +1,67 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 
 export class LiffCdkSampleStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const liffAppBucket = new s3.Bucket(this, 'LiffAppBucket', {
+      websiteIndexDocument: 'index.html',
+      websiteErrorDocument: 'index.html'
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'LiffCdkSampleQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const liffAppIdentity = new cloudfront.OriginAccessIdentity(this, 'LiffAppIdentity');
+
+    const liffAppBucketPolicyStatement = new iam.PolicyStatement({
+      actions: ['s3:GetObject'],
+      effect: iam.Effect.ALLOW,
+      principals: [liffAppIdentity.grantPrincipal],
+      resources: [`${liffAppBucket.bucketArn}/*`]
+    });
+
+    liffAppBucket.addToResourcePolicy(liffAppBucketPolicyStatement);
+
+    const liffAppDistribution = new cloudfront.CloudFrontWebDistribution(this, 'LiffAppDistribution', {
+      errorConfigurations: [
+        {
+          errorCode: 403,
+          errorCachingMinTtl: 300,
+          responseCode: 200,
+          responsePagePath: '/index.html',
+        },
+        {
+          errorCode: 404,
+          errorCachingMinTtl: 300,
+          responseCode: 200,
+          responsePagePath: '/index.html',
+        }
+      ],
+      originConfigs: [
+        {
+          s3OriginSource: {
+            s3BucketSource: liffAppBucket,
+            originAccessIdentity: liffAppIdentity
+          },
+          behaviors: [
+            {
+              isDefaultBehavior: true,
+            }
+          ]
+        }
+      ],
+      priceClass: cloudfront.PriceClass.PRICE_CLASS_ALL
+    })
+
+    new s3deploy.BucketDeployment(this, 'LiffAppDeploy', {
+      sources: [s3deploy.Source.asset('./liff-app/dist')],
+      destinationBucket: liffAppBucket,
+      distribution: liffAppDistribution,
+      distributionPaths: ['/*']
+    })
+
   }
 }
